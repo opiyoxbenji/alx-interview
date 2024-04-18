8d2ab4ba07fa5d957cf90a45bab4ad8d194856cd
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-18 08:12:11 +0000

    final code

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Determines if all boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Checks if all the boxes can be opened
+    """
+    if not boxes:
+        return False
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    return all(visited)
