c9256beaa004af4d9f1fd35c73c893a2dd94e2b7
opiyoxbenji <opiyosbenji@gmail.com>
2024-05-09 07:56:57 +0000

    utf validation fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,42 @@
 #!/usr/bin/python3
 """
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
+Task 0 - UTF-8 Validation
 """
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function to
-    convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
 
 
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+    number_bytes = 0
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-            if byte[0:2] != [1, 0]:
-                return False
-    return True
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
