38f4aa4d372dd170d33b1d87fa82863e194c18ff
opiyoxbenji <opiyosbenji@gmail.com>
2024-05-09 07:26:20 +0000

    utf validation fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,11 +8,12 @@ from itertools import takewhile
 
 def int_to_bits(nums):
     """
-    Helper function to convert ints to bits
+    Helper function to
+    convert ints to bits
     """
     for num in nums:
         bits = []
-        mask = 1 << 8
+        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
         while mask:
             mask >>= 1
             bits.append(bool(num & mask))
@@ -21,20 +22,24 @@ def int_to_bits(nums):
 
 def validUTF8(data):
     """
-    Validates if a list of ints represents a valid UTF-8 encoding
+    Takes a list of ints and returns true if the list is
+    a valid UTF-8 encoding, else returns false
     Args:
-        data: List of ints representing possible UTF-8 encoding
+        data : List of ints representing possible UTF-8 encoding
     Return:
-        bool: True if the list is a valid UTF-8 encoding, False otherwise
+        bool : True or False
     """
     bits = int_to_bits(data)
     for byte in bits:
+        # if single byte char, then valid. continue
         if byte[0] == 0:
             continue
+
+        # if here, byte is multi-byte char
         ones = sum(takewhile(bool, byte))
         if ones <= 1:
             return False
-        if ones >= 4:
+        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
             return False
 
         for _ in range(ones - 1):
