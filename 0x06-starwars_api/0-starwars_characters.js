f419583648d11c425237dd121ac26c415dbb1185
pixelbushi <opiyosbenji@gmail.com>
2024-05-23 15:21:23 +0300

    fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,35 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
   request(actors[x], function (err, res, body) {
-    if (err) throw err;
+    if (err) {
+      console.error(`Error fetching character ${actors[x]}:`, err);
+      return;
+    }
     console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);	  
+    exactOrder(actors, x + 1);
   });
 };
+
+const main = () => {
+  const filmId = process.argv[2];
+  if (!filmId) {
+    console.error('Please provide a film ID as the argument.');
+    return;
+  }
+
+  const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+  request(url, function (err, res, body) {
+    if (err) {
+      console.error('Error fetching film data:', err);
+      return;
+    }
+    const actors = JSON.parse(body).characters;
+    exactOrder(actors, 0);
+  });
+};
+
+main();
+
